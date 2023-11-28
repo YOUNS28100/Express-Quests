14abf545c74a5b352785f2eaf26d29554995ee30
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-11-28 22:49:07 +0100

    quest 02 with post on route api/users DONE

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const usersController = require("./controllers/usersController");
 
 app.get("/api/movies", movieControllers.getMovies);
+app.post("/api/movies", movieControllers.addMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 
-app.get("/api/users", usersController.getUsers)
-app.get("/api/users/:id", usersController.getUsersById)
+app.get("/api/users", usersController.getUsers);
+app.post("/api/users", usersController.addUser);
+app.get("/api/users/:id", usersController.getUsersById);
 
 module.exports = app;
