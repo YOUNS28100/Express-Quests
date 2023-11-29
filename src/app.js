3553023730b76fbea74feed2850a6bdfd01c7cc6
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-11-29 20:04:31 +0100

    testing

--- src/app.js
+++ src/app.js
@@ -5,15 +5,17 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const usersController = require("./controllers/usersController");
+const { validateMovie } = require("./middlewares/validateMovie");
+const { validateUser } = require("./middlewares/validateUser");
 
 app.get("/api/movies", movieControllers.getMovies);
-app.post("/api/movies", movieControllers.addMovie);
+app.post("/api/movies", validateMovie, movieControllers.addMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
-app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
 
 app.get("/api/users", usersController.getUsers);
-app.post("/api/users", usersController.addUser);
+app.post("/api/users", validateUser, usersController.addUser);
 app.get("/api/users/:id", usersController.getUsersById);
-app.put("/api/users/:id", usersController.updateUser);
+app.put("/api/users/:id", validateUser, usersController.updateUser);
 
 module.exports = app;
---
+++ src/middlewares/validateUser.js
@@ -0,0 +1,88 @@
+const validateUser = (req, res, next) => {
+  const { firstname, lastname, email, city, language } = req.body;
+  const errors = [];
+  const emailRegex = /[a-z0-9._]+@[a-z0-9-]+\.[a-z]{2,3}/;
+
+  if (firstname == null) {
+    errors.push({ field: "firstname", message: "This field is required" });
+  }
+  if (typeof firstname !== "string") {
+    errors.push({
+      field: "firstname",
+      message: "This type of field is not correct",
+    });
+  }
+  if (firstname.length >= 255) {
+    errors.push({
+      field: "firstname",
+      message: "Should contain less than 255 characters",
+    });
+  }
+  if (lastname == null) {
+    errors.push({ field: "lastname", message: "This field is required" });
+  }
+  if (typeof lastname !== "string") {
+    errors.push({
+      field: "lastname",
+      message: "This type of field is not correct",
+    });
+  }
+  if (lastname.length >= 255) {
+    errors.push({
+      field: "lastname",
+      message: "Should contain less than 255 characters",
+    });
+  }
+
+  if (email == null) {
+    errors.push({ field: "year", message: "This field is required" });
+  }
+  if (typeof email !== "string") {
+    errors.push({
+      field: "email",
+      message: "This type of field is not correct",
+    });
+  }
+  if (!emailRegex.test(email)) {
+    errors.push({ field: "email", message: "Invalid email" });
+  }
+  if (email.length >= 255) {
+    errors.push({
+      field: "email",
+      message: "Should contain less than 255 characters",
+    });
+  }
+
+  if (city.length >= 255) {
+    errors.push({
+      field: "city",
+      message: "Should contain less than 255 characters",
+    });
+  }
+  if (typeof city !== "string") {
+    errors.push({
+      field: "city",
+      message: "This type of field is not correct",
+    });
+  }
+  if (typeof language !== "string" && typeof language !== null) {
+    errors.push({
+      field: "language",
+      message: "This type of field is not correct",
+    });
+  }
+  if (language.length >= 255) {
+    errors.push({
+      field: "language",
+      message: "Should contain less than 255 characters",
+    });
+  }
+
+  if (errors.length) {
+    res.status(422).json({ validationErrors: errors });
+  } else {
+    next();
+  }
+};
+
+module.exports = { validateUser };
