8824b905dc0724228774bef9bd7fa155d3eee377
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-11-29 11:00:37 +0100

    quest 03 DONE with route PUT

--- src/app.js
+++ src/app.js
@@ -9,9 +9,11 @@ const usersController = require("./controllers/usersController");
 app.get("/api/movies", movieControllers.getMovies);
 app.post("/api/movies", movieControllers.addMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+app.put("/api/movies/:id", movieControllers.updateMovie);
 
 app.get("/api/users", usersController.getUsers);
 app.post("/api/users", usersController.addUser);
 app.get("/api/users/:id", usersController.getUsersById);
+app.put("/api/users/:id", usersController.updateUser);
 
 module.exports = app;
