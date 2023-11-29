6b4869caf0c0426bcf383d202725206a381a0f2d
Jean-Francois <hafidjeanfrancois@gmail.com>
2023-11-29 20:35:59 +0100

    quest 04 DONE with DELETE

--- src/app.js
+++ src/app.js
@@ -12,10 +12,12 @@ app.get("/api/movies", movieControllers.getMovies);
 app.post("/api/movies", validateMovie, movieControllers.addMovie);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie);
 
 app.get("/api/users", usersController.getUsers);
 app.post("/api/users", validateUser, usersController.addUser);
 app.get("/api/users/:id", usersController.getUsersById);
 app.put("/api/users/:id", validateUser, usersController.updateUser);
+app.delete("/api/users/:id", usersController.deleteUser);
 
 module.exports = app;
